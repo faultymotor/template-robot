2d3ee2ce6415f8781af1e5c421e25c2484ea44be
Rishi Nath <rishi.nath@gmail.com>
2019-02-16 07:50:58 -0800

    Skeleton robot

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,37 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+
+  //Runs once when the robot starts up.
+  @Override
+  public void robotInit() {
+  }
+
+  //Runs every tick when the robot is on. It does not matter which mode the robot is in (autonomous, teleoperated, etc.)
+  @Override
+  public void robotPeriodic() {
+  }
+
+  //Runs once when autonomous starts.
+  @Override
+  public void autonomousInit() {
+  }
+
+  //Runs every tick during autonomous.
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  //Runs once when teleoperated starts.
+  @Override
+  public void teleopInit(){
+  }
+
+  //Runs every tick during teleoperated.
+  @Override
+  public void teleopPeriodic() {
+  }
+
+}
