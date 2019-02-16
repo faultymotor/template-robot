d1772e5af7ba000d768bee07c6f6e09d3547020f
Rishi Nath <rishi.nath@gmail.com>
2019-02-16 09:00:38 -0800

    Added Drive command

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -1,4 +1,32 @@
 package frc.robot.commands;
 
-public class Drive {
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.OI;
+import frc.robot.subsystems.Drivetrain;
+
+public class Drive extends Command {
+
+    private OI oi;
+    private Drivetrain drivetrain;
+
+    //Getting OI and Drivetrain instances to run commands.
+    public Drive(){
+        oi = OI.getInstance();
+        drivetrain = Drivetrain.getInstance();
+        requires(drivetrain);
+    }
+
+    //Execute runs when the robot is active. Here we call the setSpeed function from the Drivetrain Class, using values from the OI class's Joysticks.
+    public void execute(){
+
+        drivetrain.setSpeed(oi.getLeftJoy(), oi.getRightJoy());
+
+    }
+
+    @Override
+    protected boolean isFinished() {
+
+        return false;
+
+    }
 }
